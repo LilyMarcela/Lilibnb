85dd9f4b2c0fef4feebadad3f39118b7371a8c83
Lili <marcela.b.abril@gmail.com>
2018-06-22 16:04:44 -0500

    add availability to booking path

--- app/controllers/place_users_controller.rb
+++ app/controllers/place_users_controller.rb
@@ -1,22 +1,31 @@
 class PlaceUsersController < ApplicationController
 
     def create
-
+       
        place_user = PlaceUser.new(place_user_params)
        #booking = Booking.new(booking_params)
        place_user.place_id = params[:place_id]
        #booking.place_id = params[:place_id]
-       place_user.user_id = current_user.id
-       #booking.user_id = current_user.id
-       place_user.save!
-       #booking.save
-       redirect_to "/places"
+       if current_user.nil?
+         redirect_to "/users/sign_in"
+       else 
+         place_user.user_id = current_user.id
+         if available?
+            #booking.user_id = current_user.id       
+            place_user.save!
+            #booking.save
+          end
+          redirect_to "/places"
+        end
        
  
     end
 
-    def availability
-         @place.where("date >= ? AND date <= ?", date_start, date_end)
+    def available?
+         PlaceUser.where(place_id: params[:place_id]).where("? BETWEEN 
+          date_start AND date_end OR 
+          ? BETWEEN date_start AND date_end", place_user_params[:date_start], 
+          place_user_params[:date_end]).none?
 
     end
 
--- app/models/place.rb
+++ app/models/place.rb
@@ -5,4 +5,9 @@ class Place < ApplicationRecord
     has_many :users, :through => :place_users
     belongs_to :host, class_name: 'User', foreign_key: :host_id
 
+
+    def available?(date = Date.today)
+        PlaceUser.where(place_id: id)
+        .where("? BETWEEN date_start AND date_end", date).none?
+    end
 end
